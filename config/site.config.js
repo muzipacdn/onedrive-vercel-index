07fcc9c41818cf3e1669a29e92e4fbf31032bfc8
Muzipa <104483047+muzipacdn@users.noreply.github.com>
2022-05-18 18:28:49 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/android-chrome-512x512.png',
 
   // The name of your website. Present alongside your icon.
-  title: "MZPA OneDrive",
+  title: "Muzipa OneDrive Storage",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: [''],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
